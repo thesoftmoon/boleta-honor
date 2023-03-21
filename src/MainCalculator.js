1bad71fe92b324c7a78bb1aa3384a460fc69b74c
Tomás <tomas.pacheco.c@gmail.com>
2023-03-21 17:20:00 -0300

    main logic added

--- src/MainCalculator.js
+++ src/MainCalculator.js
@@ -1,18 +1,40 @@
+import {useState} from 'react';
+
 function MainCalculator() {
+
+  const [mainValue, setMainValue] = useState('');
+  const [total, setTotal] = useState();
+
+  // descuento actual es 13%
+
+  const descuento = 0.13;
+
+  const calculateValue = ()=>{
+    setTotal(mainValue + (mainValue * descuento));
+    setMainValue('');
+  }
+
+
+
   return (
     <div className="row d-flex justify-content-center align-items-center content">
 
       <div className="col-12 col-md-8">
-        <div class="card px-3 px-md-5 pt-3 text-center">
-          <div class="input-group mb-3">
+        <div className="card px-3 px-md-5 pt-3 text-center">
+          <div className="input-group mb-3">
             <input
-              type="text"
-              class="form-control"
+              className="form-control"
+              type='number'
               placeholder="Ingresa tu cifra"
+              onChange={(e)=>setMainValue(+e.target.value)}
+              value={mainValue}
+              
             />
+            
             <button
-              class="btn btn-outline-secondary"
+              className="btn btn-outline-secondary"
               type="button"
+              onClick={calculateValue}
             >
               Calcular
             </button>
@@ -21,6 +43,8 @@ function MainCalculator() {
             
             <h4>Liquido</h4>
             <h4>Bruto</h4>
+            <p>{mainValue}</p>
+            <p>{total}</p>
 
             {/* Fin respuesta calculos */}
         </div>
