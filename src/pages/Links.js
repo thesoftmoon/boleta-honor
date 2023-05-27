b265ad60d4d930493995226a26762d954ede3e35
Tomás <tomas.pacheco.c@gmail.com>
2023-05-27 19:13:30 -0400

    added links structure

--- src/pages/Links.js
+++ src/pages/Links.js
@@ -1,12 +1,111 @@
-import React from 'react'
-import MainLayout from '../layout/MainLayout'
+import React from "react";
+import MainLayout from "../layout/MainLayout";
 
 function LinksPage() {
   return (
     <MainLayout>
-        <div>LinksPage</div>
+      <div className="container margin-top-pages">
+        {/* card A start */}
+        <div className="row d-flex justify-content-center mt-5">
+          <div className="col-12 col-md-8 d-md-flex justify-content-center align-items-center">
+            <div className="col-12 col-md-6 me-md-2 me-0 d-md-block d-none mt-3 mt-md-0 ms-0 ms-md-4">
+              <h2>Lorem ipsum</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                ultrices, nisl in blandit feugiat, quam nisl eleifend augue, non
+                rhoncus.
+              </p>
+              <a href="/faq" className="btn-main">
+                FAQ
+              </a>
+            </div>
+            <div className="col-12 col-md-6">
+              <img
+                src="https://dummyimage.com/600x400/000/fff"
+                class="img-fluid"
+                alt="..."
+              />
+            </div>
+            <div className="col-12 col-md-6 me-md-2 me-0 d-md-none d-block mt-3 mt-md-0 ms-0 ms-md-4">
+              <h2>Lorem ipsum</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                ultrices, nisl in blandit feugiat, quam nisl eleifend augue, non
+                rhoncus.
+              </p>
+              <a href="/faq" className="btn-main">
+                FAQ
+              </a>
+            </div>
+          </div>
+        </div>
+        {/* card A end */}
+
+        {/* card B start */}
+        <div className="row d-flex justify-content-center mt-5">
+          <div className="col-12 col-md-8 d-md-flex justify-content-center align-items-center">
+            <div className="col-12 col-md-6">
+              <img
+                src="https://dummyimage.com/600x400/000/fff"
+                class="img-fluid"
+                alt="..."
+              />
+            </div>
+            <div className="col-12 col-md-6 me-md-2 me-0 mt-3 mt-md-0 ms-0 ms-md-4">
+              <h2>Lorem ipsum</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                ultrices, nisl in blandit feugiat, quam nisl eleifend augue, non
+                rhoncus.
+              </p>
+              <a href="/faq" className="btn-main">
+                FAQ
+              </a>
+            </div>
+          </div>
+        </div>
+        {/* card B end */}
+
+        {/* card A start */}
+        <div className="row d-flex justify-content-center mt-5">
+          <div className="col-12 col-md-8 d-md-flex justify-content-center align-items-center">
+            <div className="col-12 col-md-6 me-md-2 me-0 d-md-block d-none mt-3 mt-md-0 ms-0 ms-md-4">
+              <h2>Lorem ipsum</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                ultrices, nisl in blandit feugiat, quam nisl eleifend augue, non
+                rhoncus.
+              </p>
+              <a href="/faq" className="btn-main">
+                FAQ
+              </a>
+            </div>
+            <div className="col-12 col-md-6">
+              <img
+                src="https://dummyimage.com/600x400/000/fff"
+                class="img-fluid"
+                alt="..."
+              />
+            </div>
+            <div className="col-12 col-md-6 me-md-2 me-0 d-md-none d-block mt-3 mt-md-0 ms-0 ms-md-4">
+              <h2>Lorem ipsum</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                ultrices, nisl in blandit feugiat, quam nisl eleifend augue, non
+                rhoncus.
+              </p>
+              <a href="/faq" className="btn-main">
+                FAQ
+              </a>
+            </div>
+          </div>
+        </div>
+        {/* card A end */}
+
+        
+      </div>
     </MainLayout>
-  )
+  );
 }
 
-export default LinksPage
+export default LinksPage;
